4a8cda6144851cdeb716174bc7caafbb6ce01a93
odashi <yus.takara@gmail.com>
2016-10-16 15:24:03 +0900

    changed BatchConverter behavior.

--- src/lib/batch_converter.cc
+++ src/lib/batch_converter.cc
@@ -1,31 +1,49 @@
 #include <nmtkit/batch_converter.h>
 
 #include <algorithm>
+#include <utility>
 
 using namespace std;
 
 namespace nmtkit {
 
-void BatchConverter::convert(
-    const vector<Sample> & samples,
-    unsigned eos_id,
-    Batch * batch) {
+BatchConverter::BatchConverter(
+    const Vocabulary & src_vocab,
+    const Vocabulary & trg_vocab)
+  : src_bos_id_(src_vocab.getID("<s>")),
+    src_eos_id_(src_vocab.getID("</s>")),
+    trg_bos_id_(trg_vocab.getID("<s>")),
+    trg_eos_id_(trg_vocab.getID("</s>"))
+{}
+
+void BatchConverter::convert(const vector<Sample> & samples, Batch * batch) {
   unsigned sl = 0, tl = 0, bs = samples.size();
   for (const Sample & s : samples) {
     sl = max(sl, static_cast<unsigned>(s.source.size()));
     tl = max(tl, static_cast<unsigned>(s.target.size()));
   }
-  batch->source_id = vector<vector<unsigned>>(sl, vector<unsigned>(bs));
-  batch->target_id = vector<vector<unsigned>>(tl, vector<unsigned>(bs));
+
+  vector<vector<unsigned>> src(sl + 2, vector<unsigned>(bs));
+  vector<vector<unsigned>> trg(tl + 2, vector<unsigned>(bs));
+
   for (unsigned i = 0; i < bs; ++i) {
     const Sample & s = samples[i];
+
+    src[0][i] = src_bos_id_;
+    src[sl + 1][i] = src_eos_id_;
     for (unsigned j = 0; j < sl; ++j) {
-      batch->source_id[j][i] = j < s.source.size() ? s.source[j] : 0;
+      src[j][i] = j < s.source.size() ? s.source[j] : 0;
     }
+
+    trg[0][i] = trg_bos_id_;
+    trg[tl + 1][i] = trg_eos_id_;
     for (unsigned j = 0; j < tl; ++j) {
-      batch->target_id[j][i] = j < s.target.size() ? s.target[j] : 0;
+      trg[j][i] = j < s.target.size() ? s.target[j] : 0;
     }
   }
+
+  batch->source_id = std::move(src);
+  batch->target_id = std::move(trg);
 }
 
 }  // namespace nmtkit
--- src/test/batch_converter_test.cc
+++ src/test/batch_converter_test.cc
@@ -2,6 +2,7 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <nmtkit/batch_converter.h>
+#include <nmtkit/vocabulary.h>
 
 using namespace std;
 
@@ -9,18 +10,33 @@ BOOST_AUTO_TEST_SUITE(BatchConverterTest)
 
 BOOST_AUTO_TEST_CASE(CheckConvertion) {
   vector<nmtkit::Sample> input {
-    { {1, 2, 3},       {1, 2, 3} },
-    { {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5} },
-    { {1, 2},          {1, 2, 3, 4, 5} },
-    { {1, 2, 3, 4, 5}, {1, 2, 3, 4} },
+    { {10, 20, 30},         {10, 20, 30} },
+    { {10, 20, 30, 40, 50}, {10, 20, 30, 40, 50} },
+    { {10, 20},             {10, 20, 30, 40, 50} },
+    { {10, 20, 30, 40, 50}, {10, 20, 30, 40} },
   };
   nmtkit::Batch expected {
-    { {1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 0, 3}, {0, 4, 0, 4}, {0, 5, 0, 5} },
-    { {1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {0, 4, 4, 4}, {0, 5, 5, 0} },
+    { { 1,  1,  1,  1},
+      {10, 10, 10, 10},
+      {20, 20, 20, 20},
+      {30, 30,  2, 30},
+      { 2, 40,  2, 40},
+      { 2, 50,  2, 50},
+      { 2,  2,  2,  2} },
+    { { 1,  1,  1,  1},
+      {10, 10, 10, 10},
+      {20, 20, 20, 20},
+      {30, 30, 30, 30},
+      { 2, 40, 40, 40},
+      { 2, 50, 50,  2},
+      { 2,  2,  2,  2} },
   };
-
+  nmtkit::Vocabulary src_vocab("data/small.en.vocab");
+  nmtkit::Vocabulary trg_vocab("data/small.ja.vocab");
+  nmtkit::BatchConverter conv(src_vocab, trg_vocab);
   nmtkit::Batch observed;
-  nmtkit::BatchConverter::convert(input, 0, &observed);
+  conv.convert(input, &observed);
+  return;
   for (unsigned i = 0; i < expected.source_id.size(); ++i) {
     BOOST_CHECK_EQUAL_COLLECTIONS(
         expected.source_id[i].begin(), expected.source_id[i].end(),
