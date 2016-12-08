318c8f4b2ee1faca17fc3c45f136504293bf57e9
odashi <yus.takara@gmail.com>
2016-12-08 16:16:36 +0900

    fixed passing voabulary parameters.

--- src/bin/train.cc
+++ src/bin/train.cc
@@ -328,7 +328,7 @@ int main(int argc, char * argv[]) {
     auto encoder = nmtkit::Factory::createEncoder(
         config.get<string>("Model.encoder_type"),
         config.get<unsigned>("Model.num_layers"),
-        config.get<unsigned>("Model.source_vocabulary_size"),
+        src_vocab->size(),
         config.get<unsigned>("Model.source_embedding_size"),
         config.get<unsigned>("Model.encoder_hidden_size"),
         &model);
@@ -341,7 +341,7 @@ int main(int argc, char * argv[]) {
     auto decoder = nmtkit::Factory::createDecoder(
         config.get<string>("Model.decoder_type"),
         config.get<unsigned>("Model.num_layers"),
-        config.get<unsigned>("Model.target_vocabulary_size"),
+        trg_vocab.size(),
         config.get<unsigned>("Model.target_embedding_size"),
         config.get<unsigned>("Model.output_embedding_size"),
         config.get<unsigned>("Model.decoder_hidden_size"),
