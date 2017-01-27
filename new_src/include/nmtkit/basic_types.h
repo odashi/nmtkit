0b4965e0266eaa910ada047adf7a9fa4ac3ffcd7
odashi <yus.takara@gmail.com>
2017-01-28 07:14:37 +0900

    add c-tors, substitution, comparator of Token/Sentence.

--- new_src/include/nmtkit/basic_types.h
+++ new_src/include/nmtkit/basic_types.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <utility>
 
 namespace nmtkit {
 
@@ -12,21 +13,68 @@ typedef std::unordered_map<std::string, std::string> FeatureMap;
 
 // Structure to represent one token.
 struct Token {
+  // Surface text of the token.
   std::string surface;
+
+  // Any-type features.
   FeatureMap features;
+
+  Token()
+    : surface(), features() {}
+  Token(const Token & src)
+    : surface(src.surface), features(src.features) {}
+  Token(Token && src)
+    : surface(std::move(src.surface)), features(std::move(src.features)) {}
+  explicit Token(const std::string & surface_)
+    : surface(surface_), features() {}
+  Token(const std::string & surface_, const FeatureMap & features_)
+    : surface(surface_), features(features_) {}
+
+  Token & operator=(const Token & src) {
+    surface = src.surface;
+    features = src.features;
+  }
+  Token & operator=(Token && src) {
+    surface = std::move(src.surface);
+    features = std::move(src.features);
+  }
+
+  bool operator==(const Token & lhs) {
+    return surface == src.surface && features == src.features;
+  }
 };
 
 // Structure to represent one sentence.
 struct Sentence {
+  // List of tokens in the sentence by head-to-tail order.
   std::vector<Token> tokens;
-  FeatureMap features;
-};
 
-// Structure to represent one sentence pair.
-struct SentencePair {
-  Sentence source;
-  Sentence target;
+  // Any-type features.
   FeatureMap features;
+
+  Sentence()
+    : tokens(), features() {}
+  Sentence(const Sentence & src)
+    : tokens(src.tokens), features(src.features) {}
+  Sentence(Sentence && src)
+    : tokens(std::move(src.tokens)), features(std::move(src.features)) {}
+  explicit Sentence(const std::vector<Token> & tokens_)
+    : tokens(tokens_), features() {}
+  Sentence(const std::vector<Token> & tokens_, const FeatureMap & features)
+    : tokens(tokens_), features(features_) {}
+
+  Sentence & operator=(const Sentence & src) {
+    tokens = src.tokens;
+    features = src.features;
+  }
+  Sentence & operator=(Sentence && src) {
+    tokens = std::move(src.tokens);
+    features = std::move(src.features);
+  }
+
+  bool operator==(const Sentence & lhs) {
+    return tokens == src.tokens && features == src.features;
+  }
 };
 
 }  // namespace nmtkit
